4d85c82b145a113adf060cc598c2ce79814fe951
Bernardo <bernardo_pprz@comunidad.unam.mx>
2022-01-09 15:14:51 -0600

    add Comments and fix grammar errors

--- src/App.java
+++ src/App.java
@@ -3,25 +3,26 @@ import java.util.Scanner;
 
 public class App {
     static ArrayList<Integer> list = new ArrayList<>();
-    static Boolean cicle = true;
+    static Boolean cycle = true;
 
     static void getInput(){
         System.out.println("\n*** Enter your list ***" + "\n" +
                            "For close the input enter '001' \n");
+
         Scanner console = new Scanner(System.in);
-        int input=0;
+        int input=0; // This variable we use in the conditional 
         do{
             System.out.print("Enter a number: ");
             input = console.nextInt();
             if(input!=001)
                 list.add(input);
             else
-                cicle=false;
-        }while(cicle);
+                cycle=false; // Break cycle
+        }while(cycle);
         console.close();
     }
     public static void main(String[] args) throws Exception {
-        System.out.println("*** QuickSort Parallel Algorithm ***");
+        System.out.println("*** QuickSort Parallel Algorithm ***"); // Tittle of Program
         getInput();
     }
 }
