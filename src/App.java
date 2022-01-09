5fe38f64ef9c7a9af2ca6be9b74510907c39f39f
Bernardo <bernardo_pprz@comunidad.unam.mx>
2022-01-09 15:11:13 -0600

    Add getInput

--- src/App.java
+++ src/App.java
@@ -1,6 +1,27 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class App {
+    static ArrayList<Integer> list = new ArrayList<>();
+    static Boolean cicle = true;
+
+    static void getInput(){
+        System.out.println("\n*** Enter your list ***" + "\n" +
+                           "For close the input enter '001' \n");
+        Scanner console = new Scanner(System.in);
+        int input=0;
+        do{
+            System.out.print("Enter a number: ");
+            input = console.nextInt();
+            if(input!=001)
+                list.add(input);
+            else
+                cicle=false;
+        }while(cicle);
+        console.close();
+    }
     public static void main(String[] args) throws Exception {
         System.out.println("*** QuickSort Parallel Algorithm ***");
-        
+        getInput();
     }
 }
